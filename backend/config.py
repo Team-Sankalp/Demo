9db1b2dec1d2c091ebafa90ef40d0aa22b341d31
klu2200031072 <2200031072@kluniversity.in>
2025-09-13 07:33:21 +0530

    ch

--- backend/config.py
+++
@@ -1,6 +0,0 @@
-DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",             # your MySQL username
-    "password": "GSK&ydk1",     # your password
-    "database": "telecom"       # your database name
-}
